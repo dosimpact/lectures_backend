2042705fbdf3259aaa4bce797c03df177fc1f18e
dosimpact <ypd03008@gmail.com>
2022-06-08 22:25:20 +0900

    .

--- 9.Spring/spring-kt-1/build.gradle.kts
+++ 9.Spring/spring-kt-1/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.hello-kt"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
 	mavenCentral()
@@ -31,7 +31,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+		jvmTarget = "1.8"
 	}
 }
 
