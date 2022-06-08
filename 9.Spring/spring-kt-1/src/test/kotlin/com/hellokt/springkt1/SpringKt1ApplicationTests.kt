feabc38d562827066bb11ccf83dd17c61b71b9a3
dosimpact <ypd03008@gmail.com>
2022-06-08 22:09:16 +0900

    init spring-kt

---
+++ 9.Spring/spring-kt-1/src/test/kotlin/com/hellokt/springkt1/SpringKt1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hellokt.springkt1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKt1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
