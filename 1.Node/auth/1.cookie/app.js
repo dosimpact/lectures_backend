29c43972958ff92685294883a73feacd4cfa09b4
dosimpact <ypd03008@gmail.com>
2022-07-18 14:18:37 +0000

    cookie recap

--- 1.Node/auth/1.cookie/app.js
+++ 1.Node/auth/1.cookie/app.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const PORT = 4000;
+const PORT = 2000;
 
 // 쿠키 옵션 설정
 const cookieConfig = {
   httpOnly: true,
   maxAge: 1000000,
-  signed: true, // 서명된 쿠키 , secret 필요
+  expires: null,
+  signed: true, // 서명된 쿠키 , secret 필요,
+  secure: null,
+  httpOnly: null,
+  path: null,
+  domin: null,
 };
 
 const bootstrap = async () => {
   const app = express();
   app.use(cookieParser());
 
+  // eg) cookie setter
   app.get("/", (req, res) => {
     console.log("Cookies: ", req.cookies);
     res.cookie("my-cookie", "banana", { signed: false });
     res.send(req.cookies);
   });
 
+  // eg) cookie, 기간설정
+  // non-permanent 5초뒤 사라짐
   app.get("/permanent", (req, res) => {
     console.log("permanent Cookies: ", req.cookies);
-    res.cookie("non-permanent", "short");
+    res.cookie("non-permanent", "short", {
+      // maxAge: 1000 * 5,
+      expires: new Date(Date.now() + 1000 * 5),
+    });
     res.cookie("permanent", "longlong", {
       maxAge: 60 * 60 * 24,
       expires: new Date(Date.now() + 900000),
@@ -29,26 +40,57 @@ const bootstrap = async () => {
     res.send(req.cookies);
   });
 
+  // eg) 시큐어 옵션
+  // https에서만 secure-info 쿠키가 보일것이다.
   app.get("/secure", (req, res) => {
-    res.cookie("non-secure-info", "non-secure-value", {});
-    res.cookie("secure-info", "secure-value", { secure: true });
+    res.cookie("non-secure-key", "non-secure-value", {});
+    res.cookie("secure-key", "secure-value", { secure: true });
     res.send(req.cookies);
   });
 
+  // eg) httponly 옵션
+  // js로 쿠키 변조 접근 불가
   app.get("/httponly", (req, res) => {
-    res.cookie("non-httponly-info", "non-httponly-value", {});
-    res.cookie("httponly-info", "httponly-value", { httpOnly: true });
+    res.cookie("non-httponly-key", "non-httponly-value", {});
+    res.cookie("httponly-key", "httponly-value", { httpOnly: true });
+    res.send(req.cookies);
+  });
+
+  // eg) 하위 경로
+  // o2.org           --- non-exist
+  // o2.org/path      --- ok
+  // o2.org/path/123  --- ok
+
+  app.get("/path/is-live", (req, res) => {
     res.send(req.cookies);
   });
 
   app.get("/path", (req, res) => {
-    res.cookie("non-path-info", "non-path-value", {});
-    res.cookie("path-info", "path-value", { path: "/path" });
+    res.cookie("non-path-key", "non-path-value", {});
+    res.cookie("path-key", "path-value", { path: "/path" });
     res.send(req.cookies);
   });
 
+  // eg) 도메인
+  // 테스트는 불가 (도메인구매로 해볼 순 있음)
+  // www.dodo.xyz   --- provider
+  // m.dodo.xyz     --- consumer
+  app.get("/doin", (req, res) => {
+    res.cookie("domain-key", "domain-value", { domain: "m" });
+    res.send(req.cookies);
+  });
+
+  // eg) 쿠키 삭제
   app.get("/delete", (req, res) => {
     res.clearCookie("my-cookie");
+    res.send(req.cookies);
+  });
+
+  // eg) 쿠키 카운터
+  // + refresh
+  app.get("/count", (req, res) => {
+    const current = Number(req.cookies["count"]) || 0;
+    res.cookie("count", `${current + 1}`, { maxAge: 1000 * 5 });
     res.send(req.cookies);
   });
 
