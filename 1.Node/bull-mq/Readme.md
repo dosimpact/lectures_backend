09f3710dd1261b13885bf8b70ebc19e740499741
dosimpact <dosimpact@dosimpactui-Macmini.local>
2022-12-28 11:24:52 +0900

    async-ratelimiter

--- 1.Node/bull-mq/Readme.md
+++ 1.Node/bull-mq/Readme.md
@@ -41,3 +41,53 @@ POST http://localhost:3010/retryable
 
 Recommended `REST Client` for VSC so you can use `.http` files.
 
+---
+
+## ratelimiter
+https://www.npmjs.com/package/ratelimiter
+```
+
+```
+## bottleneck
+https://www.npmjs.com/package/bottleneck#submit
+
+정확한 컨셉이해 부족, 동작방식 테스트 더 필요
+
+- bottleneck이 없는 경우 잡이 모두 통과하게 된다.
+- bottleneck이 있는 경우, minTime 만큼 기다리고 큐에 들어간다.?
+- bottleneck이 있는 경우, maxConcurrent 만큼만 처리한다. 
+
+```js
+import Bottleneck from "bottleneck";
+
+const bottleneckLimiter = new Bottleneck({
+  maxConcurrent: 1,
+  minTime: 1000,
+  id: "my-super-app",
+  datastore: "ioredis",
+  clearDatastore: false,
+  clientOptions: {
+    host: "localhost",
+    port: 5059,
+  },
+});
+
+
+const LimitedWork = async () =>
+  bottleneckLimiter.schedule(async () => {
+    await sleep(3000);
+    return getRandomInt(1, 100);
+  });
+
+  app.get("/bottle-neck", async (req, res) => {
+    const result: number[] = await Promise.all(
+      new Array(10).fill(1).map(async (_, i) => {
+        const nums = await LimitedWork();
+        console.log("[info] bottle : ", i);
+        return nums;
+      })
+    );
+    res.json({ result });
+  });
+
+```
--- 1.Node/bull-mq/server.api.ts
+++ 1.Node/bull-mq/server.api.ts
@@ -3,10 +3,15 @@ import { getRandomInt } from "./utils";
 import rateLimit, { MemoryStore } from "express-rate-limit";
 import RedisStore from "rate-limit-redis";
 import { createClient } from "redis";
+import Bottleneck from "bottleneck";
+import AsyncRateLimiter from "async-ratelimiter";
+import Redis from "ioredis";
+
+const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const bootstrap = async () => {
   const client = createClient({
-    url: "redis://localhost:5059",
+    url: "redis://localhost:5059", // redis-cli -h host -p 5059
   });
   await client.connect();
 
@@ -23,23 +28,46 @@ const bootstrap = async () => {
     }),
   });
 
+  const asyncRateLimiter = new AsyncRateLimiter({
+    duration: 3000,
+    max: 3,
+    db: new Redis({ port: 5059 }),
+  });
+
   const app = express();
 
   // EXPRESS SETUP
 
-  let inboundCnt = 0;
+  let inboundCnt = 1;
 
   app.get("/", async (_req, res) => {
     res.json({ ok: true });
   });
 
   app.get("/num", limiter, async (_req, res) => {
-    console.log("[info] inbound : ", ++inboundCnt);
+    console.log("[info] inbound : ", inboundCnt++);
+    setTimeout(() => {
+      console.log("[info] setTimeout : ", inboundCnt);
+    }, 1000);
     res.json({ num: getRandomInt(1, 100) });
   });
 
-  // STARTUP
+  app.get("/num-2", async (_req, res) => {
+    console.log("[info] inbound : ", inboundCnt++);
+    const { remaining, reset, total } = await asyncRateLimiter.get({ id: "1" });
+    console.log("[info] remaining,reset,total", remaining, reset, total);
+    if (remaining === 0) {
+      return res.json({ fail: true, remaining, reset, total });
+    }
+    /*
+      total: max value.
+      remaining: number of calls left in current duration without decreasing current get.
+      reset: time since epoch in seconds that the rate limiting period will end (or already ended).
+    */
+    return res.json({ num: getRandomInt(1, 100), remaining, reset, total });
+  });
 
+  // STARTUP
   const PORT = process.env.PORT || 5058;
 
   app.listen(PORT, () => {
@@ -47,5 +75,3 @@ const bootstrap = async () => {
   });
 };
 bootstrap();
-
-// redis-cli -h host -p 5059
