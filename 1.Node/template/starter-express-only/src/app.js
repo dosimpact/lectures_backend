74abe3a256091cad7bc131530bc02484fffd4b68
dosimpact <dosimpact@dosimpactui-Macmini.local>
2023-02-19 12:19:51 +0900

    add eg, make template

---
+++ 1.Node/template/starter-express-only/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const PORT = process.env.PORT || 4000;
+
+const bootstrap = async () => {
+  const app = express();
+
+  app.get("/", (req, res) => {
+    return res.json({ ok: true });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
+};
+
+bootstrap();
