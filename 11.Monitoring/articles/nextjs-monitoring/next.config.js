cc353b6ebb76d8d26f93e499f9a7cbfd3f48ee3b
dosimpact <ypd03008@gmail.com>
2023-08-30 16:42:09 +0900

    update monit

---
+++ 11.Monitoring/articles/nextjs-monitoring/next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
